c38860bf91d1b3c121a4f164517fcffa43529f8d
PeterKow <peter.kowalczyk@aurity.co>
2016-01-17 17:27:54 +0000

    init

---
+++ index.ios.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  NavigatorIOS,
+  View,
+} = React;
+import Main from './app/components/main'
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111'
+  }
+})
+
+var githubExample = React.createClass({
+  render: function() {
+    return (
+      <NavigatorIOS
+        style={ styles.container}
+        initialRoute={{
+        title: 'Github main',
+        component: Main
+        }}/>
+    );
+  }
+});
+
+AppRegistry.registerComponent('githubExample', () => githubExample);
